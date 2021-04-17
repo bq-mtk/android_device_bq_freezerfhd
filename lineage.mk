3abf4a9b3d9e971725f5441feaefdb800c993f43
Rashed Abdel-Tawab <rashed@linux.com>
2021-04-18 00:58:11 +0200

    freezerfhd: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -17,6 +17,7 @@
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 # Inherit from freezerfhd device
 $(call inherit-product, device/bq/freezerfhd/device.mk)
